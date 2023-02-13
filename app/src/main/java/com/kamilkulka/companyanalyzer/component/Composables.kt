f55ad30ef138d83037d2ebdbcd48ab528d27e9f2
Kamil Kulka <kulka2190@gmail.com>
2023-02-13 19:54:33 +0100

    Separate analytic responses and change of UI.
    All analytical responses were stored in one common column, it was unreadable approach. Every statistic query have to be separated.

--- app/src/main/java/com/kamilkulka/companyanalyzer/component/Composables.kt
+++ app/src/main/java/com/kamilkulka/companyanalyzer/component/Composables.kt
@@ -1,14 +1,20 @@
 package com.kamilkulka.companyanalyzer.component
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ExpandLess
+import androidx.compose.material.icons.rounded.ExpandMore
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -17,45 +23,135 @@ import com.kamilkulka.companyanalyzer.model.CustomerItem
 import java.time.LocalDate
 
 @Composable
-fun CustomerItemCard(
+fun CustomerCard(
     customerItem: CustomerItem,
     modifier: Modifier = Modifier,
     elevation: Dp = 1.dp,
     shape: Shape = MaterialTheme.shapes.medium,
     contentPadding: Dp = 1.dp
 ) {
+    var extraDataUnfolded by remember { mutableStateOf(false) }
     Card(modifier = modifier, elevation = elevation, shape = shape) {
-        Column(modifier = Modifier.padding(contentPadding)) {
-            NameAndValueRow(name = stringResource(id = R.string.first_name) , customerItem.firstName)
-            NameAndValueRow(name = stringResource(id = R.string.last_name), customerItem.lastName)
-            NameAndValueRow(name = stringResource(id = R.string.job), customerItem.job)
-            NameAndValueRow(name = stringResource(id = R.string.company), customerItem.company)
-            NameAndValueRow(name = stringResource(id = R.string.city), customerItem.city)
-            NameAndValueRow(name = stringResource(id = R.string.street), customerItem.street)
-            NameAndValueRow(name = stringResource(id = R.string.type), customerItem.type)
-            NameAndValueRow(name = stringResource(id = R.string.zip), customerItem.zip)
-            NameAndValueRow(name = stringResource(id = R.string.phone), customerItem.phone?.toString())
-            NameAndValueRow(name = stringResource(id = R.string.last_check_in_date), customerItem.lastCheckInDate?.toString())
+        Column(modifier = Modifier.padding(contentPadding).animateContentSize()) {
+            Row(modifier = Modifier.fillMaxWidth(),verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
+                VerticalValueWithTitle(
+                    title = stringResource(id = R.string.first_name),
+                    value = customerItem.firstName,
+                    contentPadding
+                )
+                VerticalValueWithTitle(
+                    title = stringResource(id = R.string.last_name),
+                    value = customerItem.lastName,
+                    contentPadding
+                )
+                VerticalValueWithTitle(
+                    title = stringResource(id = R.string.last_check_in_date),
+                    value = customerItem.lastCheckInDate?.toString(),
+                    contentPadding
+                )
+                IconButton(onClick = { extraDataUnfolded = !extraDataUnfolded }) {
+                    if (extraDataUnfolded) Icon(
+                        imageVector = Icons.Rounded.ExpandLess,
+                        contentDescription = null
+                    )
+                    else Icon(imageVector = Icons.Rounded.ExpandMore, contentDescription = null)
+                }
+            }
+            if (extraDataUnfolded) {
+                Divider(modifier = Modifier.padding(contentPadding.times(2)), thickness = 1.5.dp)
+
+                HorizontalValueWithTitle(
+                    title = stringResource(id = R.string.job),
+                    value = customerItem.job,
+                    contentPadding
+                )
+                HorizontalValueWithTitle(
+                    title = stringResource(id = R.string.company),
+                    value = customerItem.company,
+                    contentPadding
+                )
+                HorizontalValueWithTitle(
+                    title = stringResource(id = R.string.city),
+                    value = customerItem.city,
+                    contentPadding
+                )
+                HorizontalValueWithTitle(
+                    title = stringResource(id = R.string.street),
+                    value = customerItem.street,
+                    contentPadding
+                )
+                HorizontalValueWithTitle(
+                    title = stringResource(id = R.string.type),
+                    value = customerItem.type,
+                    contentPadding
+                )
+                HorizontalValueWithTitle(
+                    title = stringResource(id = R.string.zip),
+                    value = customerItem.zip,
+                    contentPadding
+                )
+                HorizontalValueWithTitle(
+                    title = stringResource(id = R.string.phone),
+                    value = customerItem.phone?.toString(),
+                    contentPadding
+                )
+            }
         }
     }
 }
 
 @Composable
-fun NameAndValueRow(name: String, value: String?) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start
+private fun VerticalValueWithTitle(title: String, value: String?, contentPadding: Dp) {
+    Column(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(contentPadding),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "${name}: ")
-        Text(text = value ?: "-")
+        Text(text = value ?: "-", maxLines = 1, overflow = TextOverflow.Ellipsis)
+        Text(
+            text = title,
+            style = MaterialTheme.typography.caption,
+            color = Color.Gray,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@Composable
+private fun HorizontalValueWithTitle(title: String, value: String?, contentPadding: Dp) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(contentPadding),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.caption,
+            color = Color.Gray,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = value ?: "-",
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.weight(2f)
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun CustomerItemCardPreview() {
-    CustomerItemCard(
+    CustomerCard(
+        elevation = 6.dp,
+        shape = RoundedCornerShape(CornerSize(12.dp)),
+        contentPadding = 4.dp,
         customerItem = CustomerItem(
             firstName = "John",
             lastName = "Doe",
--- app/src/main/java/com/kamilkulka/companyanalyzer/screens/ClientsScreen.kt
+++ app/src/main/java/com/kamilkulka/companyanalyzer/screens/ClientsScreen.kt
@@ -1,73 +1,157 @@
 package com.kamilkulka.companyanalyzer.screens
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
 import com.kamilkulka.companyanalyzer.R
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.kamilkulka.companyanalyzer.component.CustomerItemCard
+import com.kamilkulka.companyanalyzer.component.CustomerCard
+import com.kamilkulka.companyanalyzer.model.Statistics
+import kotlinx.coroutines.launch
 
 @Composable
 fun ClientsScreen(viewModel: ClientsViewModel = hiltViewModel()) {
-    var isExpandedEarliestCustomer by remember { mutableStateOf(true) }
-    var isExpandedLatestCustomer by remember { mutableStateOf(true) }
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
-    Surface(modifier = Modifier.fillMaxSize()) {
-        LazyRow(modifier=Modifier.fillMaxSize()){
-            item {
-                Column {
-                    Text(
-                        text = stringResource(id = R.string.earliest_check_in_customer),
-                        style = MaterialTheme.typography.h5,
-                        modifier = Modifier.clickable {
-                            isExpandedEarliestCustomer = !isExpandedEarliestCustomer
-                        })
-                    if (isExpandedEarliestCustomer) {
-                        LazyColumn {
+    var statistics by remember { mutableStateOf(Statistics.ALL_CUSTOMERS) }
+
+    ModalDrawer(drawerContent = {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)) {
+            OutlinedButton(onClick = { statistics = Statistics.ALL_CUSTOMERS }) {
+                Text(
+                    text = stringResource(id = R.string.all_customers),
+                    style = MaterialTheme.typography.button,
+                    color = if (statistics == Statistics.ALL_CUSTOMERS) MaterialTheme.colors.secondary else MaterialTheme.colors.primary
+                )
+            }
+            OutlinedButton(onClick = { statistics = Statistics.EARLIEST_CHECK_IN }) {
+                Text(
+                    text = stringResource(id = R.string.earliest_check_in),
+                    style = MaterialTheme.typography.button,
+                    color = if (statistics == Statistics.EARLIEST_CHECK_IN) MaterialTheme.colors.secondary else MaterialTheme.colors.primary
+                )
+            }
+            OutlinedButton(onClick = { statistics = Statistics.LATEST_CHECK_IN }) {
+                Text(
+                    text = stringResource(id = R.string.latest_check_in),
+                    style = MaterialTheme.typography.button,
+                    color = if (statistics == Statistics.LATEST_CHECK_IN) MaterialTheme.colors.secondary else MaterialTheme.colors.primary
+                )
+            }
+            OutlinedButton(onClick = { statistics = Statistics.SORTED_NAMES }) {
+                Text(
+                    text = stringResource(id = R.string.sorted_names),
+                    style = MaterialTheme.typography.button,
+                    color = if (statistics == Statistics.SORTED_NAMES) MaterialTheme.colors.secondary else MaterialTheme.colors.primary
+                )
+            }
+            OutlinedButton(onClick = { statistics = Statistics.SORTED_JOBS }) {
+                Text(
+                    text = stringResource(id = R.string.sorted_jobs),
+                    style = MaterialTheme.typography.button,
+                    color = if (statistics == Statistics.SORTED_JOBS) MaterialTheme.colors.secondary else MaterialTheme.colors.primary
+                )
+            }
+        }
+    }, drawerState = drawerState) {
+        Column {
+            TopAppBar(backgroundColor = MaterialTheme.colors.primary) {
+                IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                    Icon(imageVector = Icons.Rounded.Menu, contentDescription = "")
+                }
+
+                val title = when (statistics) {
+                    Statistics.ALL_CUSTOMERS -> stringResource(id = R.string.all_customers)
+                    Statistics.EARLIEST_CHECK_IN -> stringResource(id = R.string.earliest_check_in)
+                    Statistics.LATEST_CHECK_IN -> stringResource(id = R.string.latest_check_in)
+                    Statistics.SORTED_NAMES -> stringResource(id = R.string.sorted_names)
+                    Statistics.SORTED_JOBS -> stringResource(id = R.string.sorted_jobs)
+                }
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.h6,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            Surface(modifier = Modifier.fillMaxSize()) {
+                LazyColumn(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+                    when (statistics) {
+                        Statistics.ALL_CUSTOMERS -> {
+                            items(items = viewModel.getAllCustomers()) { customerItem ->
+                                CustomerCard(
+                                    customerItem,
+                                    modifier = Modifier.padding(6.dp),
+                                    elevation = 8.dp,
+                                    shape = RoundedCornerShape(
+                                        CornerSize(16.dp)
+                                    ),
+                                    contentPadding = 3.dp
+                                )
+                            }
+                        }
+                        Statistics.EARLIEST_CHECK_IN -> {
                             items(items = viewModel.getEarliestCustomer()) { customerItem ->
-                                CustomerItemCard(customerItem)
+                                CustomerCard(
+                                    customerItem,
+                                    modifier = Modifier.padding(6.dp),
+                                    elevation = 8.dp,
+                                    shape = RoundedCornerShape(
+                                        CornerSize(16.dp)
+                                    ),
+                                    contentPadding = 3.dp
+                                )
                             }
                         }
-                    }
-                }
-            }
-            item {
-                Column {
-                    Text(
-                        text = stringResource(id = R.string.latest_check_in_customer),
-                        style = MaterialTheme.typography.h5,
-                        modifier = Modifier.clickable {
-                            isExpandedLatestCustomer = !isExpandedLatestCustomer
-                        })
-                    if (isExpandedLatestCustomer) {
-                        LazyColumn {
+                        Statistics.LATEST_CHECK_IN -> {
                             items(items = viewModel.getLatestCustomer()) { customerItem ->
-                                CustomerItemCard(customerItem)
+                                CustomerCard(
+                                    customerItem,
+                                    modifier = Modifier.padding(6.dp),
+                                    elevation = 8.dp,
+                                    shape = RoundedCornerShape(
+                                        CornerSize(16.dp)
+                                    ),
+                                    contentPadding = 3.dp
+                                )
                             }
                         }
-                    }
-                }
-            }
-            item {
-                LazyColumn {
-                    items(items = viewModel.getAllFullNamesAlphabetically()) { name ->
-                        Text(text = name)
-                    }
-                }
-            }
-            item {
-                LazyColumn {
-                    items(items = viewModel.getAllJobsAlphabetically()) { job ->
-                        Text(text = job)
+                        Statistics.SORTED_NAMES -> {
+                            items(items = viewModel.getAllFullNamesAlphabetically()) { name ->
+                                Card(
+                                    modifier = Modifier.padding(6.dp),
+                                    elevation = 8.dp,
+                                    shape = RoundedCornerShape(CornerSize(8.dp))
+                                ) {
+                                    Text(modifier = Modifier.padding(6.dp), text = name)
+                                }
+                            }
+                        }
+                        Statistics.SORTED_JOBS -> {
+                            items(items = viewModel.getAllJobsAlphabetically()) { job ->
+                                Card(
+                                    modifier = Modifier.padding(6.dp),
+                                    elevation = 8.dp,
+                                    shape = RoundedCornerShape(CornerSize(8.dp))
+                                ) {
+                                    Text(modifier = Modifier.padding(6.dp), text = job)
+                                }
+                            }
+                        }
                     }
                 }
             }
